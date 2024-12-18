801007ae3be6db6fd07d55b2e8b29558095b3e63
Yuxin Liu <yuxin.liu@xceptor.com>
2024-12-18 13:59:14 +0800

    Restructure the project

---
+++ EndPoints/MoviesEndPoints.cs
@@ -0,0 +1,85 @@
+﻿using MovieApp.Dtos;
+
+namespace MovieApp.EndPoints
+{
+    public static class MoviesEndPoints
+    {
+
+        const string getMovieEndPoint = "GetMovie";
+        private static List<MovieDto> movies = [
+            new MovieDto(1,"The Shawshank Redemption", "Drama","Frank Darabont",14.5, new DateOnly(1994,10,14)),
+                new MovieDto(2,"The Godfather", "Crime", "Francis Ford Coppola",8.6, new DateOnly(1972,4,24)),
+                new MovieDto(3,"The Dark Knight", "Action", "Christopher Nolan", 25.5, new DateOnly(2008,7,18))
+       ];
+
+
+        public static RouteGroupBuilder MapMoviesEndPoints(this WebApplication app)
+        {
+
+            var group = app.MapGroup("Movies");
+            // GET /movies all data
+            group.MapGet("/", () => movies);
+
+            // GET /movies/1
+            group.MapGet("/{id}", (int id) =>
+            {
+                MovieDto? movieDto = movies.Find(x => x.Id == id);
+                return movieDto is null ? Results.NotFound() : Results.Ok(movieDto);
+            }).WithName(getMovieEndPoint);
+
+
+            // POST /Movies create a new movie
+            group.MapPost("/", (CreateMovieDto createMovieDto) =>
+            {
+                MovieDto movieDto = new MovieDto(
+                    movies.Count + 1,
+                    createMovieDto.Title,
+                    createMovieDto.Genre,
+                    createMovieDto.Director,
+                    createMovieDto.TicketPrice,
+                    createMovieDto.ReleaseDate
+                    );
+                movies.Add(movieDto);
+                return Results.CreatedAtRoute(getMovieEndPoint, new { id = movieDto.Id }, movieDto);
+            });
+
+            //PUT /Movies/1 update a movie
+            group.MapPut("/{id}", (int id, UpdateMovieDto updateMovieDto) =>
+            {
+                int index = movies.FindIndex(m => m.Id == id);
+                if (index == -1)
+                {
+                    return Results.NotFound();
+                }
+
+                movies[index] = new MovieDto(
+                    index + 1,
+                    updateMovieDto.Title,
+                    updateMovieDto.Genre,
+                    updateMovieDto.Director,
+                    updateMovieDto.TicketPrice,
+                    updateMovieDto.ReleaseDate
+                    );
+
+                return Results.NoContent();
+            });
+
+            //DELETE /Movies/1 delete a movie
+            group.MapDelete("/{id}", (int id) =>
+            {
+
+                movies.RemoveAll(m => m.Id == id);
+
+                return Results.NoContent();
+            });
+
+
+            return group;
+
+
+        }
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using MovieApp.Dtos;
+using MovieApp.EndPoints;
 namespace MovieApp
 {
     public class Program
@@ -7,75 +8,10 @@ namespace MovieApp
         {
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
-            const string getMovieEndPoint = "GetMovie";
+
 
             app.MapGet("/", () => "Hello World!");
-
-
-            List<MovieDto> movies = [
-                new MovieDto(1,"The Shawshank Redemption", "Drama","Frank Darabont",14.5, new DateOnly(1994,10,14)),
-                new MovieDto(2,"The Godfather", "Crime", "Francis Ford Coppola",8.6, new DateOnly(1972,4,24)),
-                new MovieDto(3,"The Dark Knight", "Action", "Christopher Nolan", 25.5, new DateOnly(2008,7,18))
-           ];
-
-
-
-            // GET /movies all data
-            app.MapGet("Movies", () => movies);
-
-            // GET /movies/1
-            app.MapGet("Movies/{id}",(int id) =>
-            {
-                MovieDto? movieDto = movies.Find(x => x.Id == id);
-                return movieDto is null ? Results.NotFound() : Results.Ok(movieDto);
-            }).WithName(getMovieEndPoint);
-
-
-            // POST /Movies create a new movie
-            app.MapPost("Movies", (CreateMovieDto createMovieDto) =>
-            {
-                MovieDto movieDto = new MovieDto(
-                    movies.Count + 1,
-                    createMovieDto.Title,
-                    createMovieDto.Genre,
-                    createMovieDto.Director,
-                    createMovieDto.TicketPrice,
-                    createMovieDto.ReleaseDate
-                    );
-                movies.Add(movieDto);
-                return Results.CreatedAtRoute(getMovieEndPoint, new { id = movieDto.Id }, movieDto);
-            });
-
-            //PUT /Movies/1 update a movie
-            app.MapPut("Movies/{id}", (int id,UpdateMovieDto updateMovieDto) =>
-            {
-                int index = movies.FindIndex(m=>m.Id == id);
-                if (index == -1) { 
-                return Results.NotFound();
-                }
-
-                movies[index] = new MovieDto(
-                    index+1,
-                    updateMovieDto.Title,
-                    updateMovieDto.Genre,
-                    updateMovieDto.Director,
-                    updateMovieDto.TicketPrice,
-                    updateMovieDto.ReleaseDate
-                    );
-                 
-                return Results.NoContent();
-            });
-
-            //DELETE /Movies/1 delete a movie
-            app.MapDelete("Movies/{id}", (int id) =>
-            {
-                
-                movies.RemoveAll(m=>m.Id==id);
-
-                return Results.NoContent();
-            });
-
-
+            app.MapMoviesEndPoints();
 
 
             app.Run();
