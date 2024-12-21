1ca64dddd2baea5766466dc574f72045cbb2eea5
Yuxin Liu <yuxin.liu@xceptor.com>
2024-12-21 17:04:06 +0800

    using bootstrap for  frontend, set up dispaly function

---
+++ MovieAppFrontEnd/MovieApp.Frontend/Clients/MoviesClient.cs
@@ -0,0 +1,32 @@
+﻿using MovieApp.Frontend.Models;
+
+namespace MovieApp.Frontend.Clients
+{
+    public class MoviesClient
+    {
+        private readonly List<MovieSummary> movies = [
+           new(){
+           Id = 1,
+           Title = "The God Father",
+           Genre = "Crime",
+           Director = "Francis Ford Coppola",
+           TicketPrice = 14.5M,
+           ReleaseDate = new DateOnly(1972,04,24)
+            },
+
+       new(){
+           Id = 2,
+           Title = "The Shawshank Redemption",
+           Genre = "Drama",
+           Director = "Frank Darabont",
+           TicketPrice = 23M,
+           ReleaseDate = new DateOnly(1994,10,14)
+            }
+
+    ];
+
+
+        public MovieSummary[] GetMovies() => [.. movies];
+
+    }
+}
