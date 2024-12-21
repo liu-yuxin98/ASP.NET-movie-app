449c700687c77dd3c7512205bb8cf317bb2c548e
Yuxin Liu <yuxin.liu@xceptor.com>
2024-12-21 16:08:55 +0800

    frontend setup

---
+++ MovieAppFrontEnd/MovieApp.Frontend/Models/MovieSummary.cs
@@ -0,0 +1,14 @@
+﻿namespace MovieApp.Frontend.Models
+{
+    public class MovieSummary
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Genre {  get; set; }
+
+        public required string Director { get; set; }
+
+        public decimal TicketPrice { get; set; }
+        public DateOnly ReleaseDate { get; set; }
+    }
+}
